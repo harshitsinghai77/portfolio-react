2443649d65d18912eac7d0fad68f4fcc46821af7
Harshit Singhai <30886142+harshitsinghai77@users.noreply.github.com>
2019-04-05 16:31:34 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ddd70fd74e226ca53ccd151ee01ad924.js"
+  "/harshitsinghai77/harshitsinghai77.github.io/precache-manifest.4f48d49e1f1d54b3802b08244e29d6e6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/harshitsinghai77/harshitsinghai77.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
