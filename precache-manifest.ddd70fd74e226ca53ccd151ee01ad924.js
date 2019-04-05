8e54cce841a992dd50bcefd178cc6e84ae7ef963
Harshit Singhai <30886142+harshitsinghai77@users.noreply.github.com>
2019-04-05 16:20:54 +0530

    Updates

---
+++ precache-manifest.ddd70fd74e226ca53ccd151ee01ad924.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "e867fe16f51d5281386f5663142f2eff",
+    "url": "/static/media/voice-interface.e867fe16.png"
+  },
+  {
+    "revision": "6ef4a64ca7818ee8ec97",
+    "url": "/static/css/main.a9faf791.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed64ecf566942be67641",
+    "url": "/static/js/2.0ea94c41.chunk.js"
+  },
+  {
+    "revision": "05b2455d22e33a66812c371773eae91f",
+    "url": "/static/media/icon-mentor.05b2455d.svg"
+  },
+  {
+    "revision": "6e024deec44cefc7a8b49aca263e72fd",
+    "url": "/static/media/icon-designer.6e024dee.svg"
+  },
+  {
+    "revision": "5f379fd2aa9c44f384b3e1799ff361fb",
+    "url": "/static/media/icon-frontend.5f379fd2.svg"
+  },
+  {
+    "revision": "f934acda49e51498d423df18b7d2858f",
+    "url": "/static/media/gan.f934acda.png"
+  },
+  {
+    "revision": "6366281ac189488ee85d364aacd2524d",
+    "url": "/static/media/portfolio.6366281a.png"
+  },
+  {
+    "revision": "d9f384e6ab8569d433978b344ef38fb1",
+    "url": "/static/media/smart-mirror.d9f384e6.png"
+  },
+  {
+    "revision": "6ef4a64ca7818ee8ec97",
+    "url": "/static/js/main.e35b6268.chunk.js"
+  },
+  {
+    "revision": "27d86f407c9f5b7d820a0f869fa097b3",
+    "url": "/static/media/webase.27d86f40.png"
+  },
+  {
+    "revision": "64826651412228ac36fd2d17858c2bdc",
+    "url": "/static/media/wester.64826651.png"
+  },
+  {
+    "revision": "615a6c9b8d6d127f746bc753fe31a7a6",
+    "url": "/static/media/misha.615a6c9b.png"
+  },
+  {
+    "revision": "f3eb6fc073459985f79b5ec722cf1d84",
+    "url": "/static/media/barco.f3eb6fc0.jpg"
+  },
+  {
+    "revision": "ab8338e0bd6e47513dae77308845303f",
+    "url": "/static/media/hackbmu.ab8338e0.jpg"
+  },
+  {
+    "revision": "6d034ad18e66774e70fc100ae0dd9de5",
+    "url": "/static/media/mlh.6d034ad1.jpg"
+  },
+  {
+    "revision": "a2794e33f5dedd2aaa03cf5169f50faa",
+    "url": "/static/media/sih.a2794e33.jpg"
+  },
+  {
+    "revision": "6f8ad97459f911930fe62959b54953d4",
+    "url": "/static/media/leadingIndia.6f8ad974.png"
+  },
+  {
+    "revision": "e37c457504d52c11a4c36193aa923eb7",
+    "url": "/static/media/opalina.e37c4575.png"
+  },
+  {
+    "revision": "4f3b48eef2ff33707795683086e21378",
+    "url": "/index.html"
+  }
+];
