7c6f833113a43f5d0b5d896b592e408a7f649ba2
harshitt <harshit@leena.ai>
2019-06-30 13:07:16 +0530

    Updates

---
+++ precache-manifest.ad0929ce7e40dcba1181eaa4912456c5.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "18f94373b1f22c319adf",
+    "url": "/static/css/main.6de15bf1.chunk.css"
+  },
+  {
+    "revision": "18f94373b1f22c319adf",
+    "url": "/static/js/main.3e859374.chunk.js"
+  },
+  {
+    "revision": "28f09c72e15eeae486a6",
+    "url": "/static/js/runtime~main.a9bdac06.js"
+  },
+  {
+    "revision": "ed39c3c6c27896057050",
+    "url": "/static/css/2.fd930217.chunk.css"
+  },
+  {
+    "revision": "ed39c3c6c27896057050",
+    "url": "/static/js/2.aa41cec4.chunk.js"
+  },
+  {
+    "revision": "94a841b6da353898dbfa",
+    "url": "/static/js/3.ac5f0b65.chunk.js"
+  },
+  {
+    "revision": "25a50cee718d453947a0",
+    "url": "/static/js/4.da3b3e8a.chunk.js"
+  },
+  {
+    "revision": "86616226a4593885bdf1",
+    "url": "/static/js/5.54678745.chunk.js"
+  },
+  {
+    "revision": "9e43497604101e4608ef",
+    "url": "/static/js/6.d12daa16.chunk.js"
+  },
+  {
+    "revision": "a6532f9eb0bef7fcbd35",
+    "url": "/static/js/7.e004355f.chunk.js"
+  },
+  {
+    "revision": "ba294cfe249730d2e6e5",
+    "url": "/static/js/8.c943b51f.chunk.js"
+  },
+  {
+    "revision": "c8ee46b3011381353702",
+    "url": "/static/js/9.66af4d12.chunk.js"
+  },
+  {
+    "revision": "1f0a544ff861c584670f",
+    "url": "/static/js/10.85918e40.chunk.js"
+  },
+  {
+    "revision": "53d0522d17cc89f47e94",
+    "url": "/static/js/11.58d79017.chunk.js"
+  },
+  {
+    "revision": "a7f44b31dd5b063e765f07f14076058d",
+    "url": "/static/media/avatar.a7f44b31.svg"
+  },
+  {
+    "revision": "dd79b27ef6811e1ae7a33f7135464a2f",
+    "url": "/static/media/portfolio-image-bg.dd79b27e.png"
+  },
+  {
+    "revision": "349da5fc4debad9d727e2fbe19b15f73",
+    "url": "/static/media/icon-mentor.349da5fc.svg"
+  },
+  {
+    "revision": "168f0f801110af0cd293ce17b9f8ebae",
+    "url": "/static/media/icon-designer.168f0f80.svg"
+  },
+  {
+    "revision": "0ee15e5fbbbe43b6ad37264f86f20c53",
+    "url": "/static/media/icon-frontend.0ee15e5f.svg"
+  },
+  {
+    "revision": "615a6c9b8d6d127f746bc753fe31a7a6",
+    "url": "/static/media/misha.615a6c9b.png"
+  },
+  {
+    "revision": "f3eb6fc073459985f79b5ec722cf1d84",
+    "url": "/static/media/barco.f3eb6fc0.jpg"
+  },
+  {
+    "revision": "ab8338e0bd6e47513dae77308845303f",
+    "url": "/static/media/hackbmu.ab8338e0.jpg"
+  },
+  {
+    "revision": "6d034ad18e66774e70fc100ae0dd9de5",
+    "url": "/static/media/mlh.6d034ad1.jpg"
+  },
+  {
+    "revision": "a2794e33f5dedd2aaa03cf5169f50faa",
+    "url": "/static/media/sih.a2794e33.jpg"
+  },
+  {
+    "revision": "6f8ad97459f911930fe62959b54953d4",
+    "url": "/static/media/leadingIndia.6f8ad974.png"
+  },
+  {
+    "revision": "e37c457504d52c11a4c36193aa923eb7",
+    "url": "/static/media/opalina.e37c4575.png"
+  },
+  {
+    "revision": "070e167c25763674a8f0e74aaae6c9cf",
+    "url": "/index.html"
+  }
+];
